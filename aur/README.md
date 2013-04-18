fc37e744c325df461636e3b24ff0788f113e2b00
Jeff <jostendorf@gmail.com>
2013-04-18 09:08:27 -0500

    Added the aur script

---
+++ aur/README.md
@@ -0,0 +1,7 @@
+# AUR
+
+This is a script that helps manage AUR packages.  These are for Arch Linux and are
+a way to install applications that are not part of the main repository.  They are
+just standardized scripts to build applications from source.  I wrote this script
+set to quickly and easily install applications from the repository and update them
+with simple commands.
---
+++ aur/aur.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+aur_get() {
+   cd $HOME/.aur/
+   if [[ -d "$1" ]]; then
+      aur_update $1
+      return
+   fi
+
+   ABBR=${1:0:2}
+   if ! wget "http://aur.archlinux.org/packages/$ABBR/$1/$1.tar.gz"; then
+      echo "Failed to retrieve package $1."
+      return 1
+   fi
+   tar -xzf "$1.tar.gz"
+   rm "$1.tar.gz"
+   cd "$1"
+   makepkg
+}
+
+aur_update() {
+   cd $HOME/.aur/
+   if [[ ! -d "$1" ]]; then
+      echo "Package $1 is not already installed."
+      return 1
+   fi
+
+   cd "$1"
+   makepkg
+}
+
+if [[ -z "${1}" ]]; then
+   echo "Missing argument (1 required)"
+   return 1
+fi
+
+case "$1" in
+   ('get'|'install')
+      aur_get $2 ;;
+   ('update'|'upgrade')
+      aur_update $2 ;;
+   (*) aur_get $1 ;;
+esac
