51ffc52775f0f1c65e42781175f2daa782e1b183
Jeff <jostendorf@gmail.com>
2013-05-12 09:47:45 -0500

    Some changes to the wallpaper script

--- wallpaper/wallpaper.sh
+++ wallpaper/wallpaper.sh
@@ -1,20 +1,22 @@
 #!/bin/zsh
 
-WP_FOLDER=$HOME/.wallpapers
-CURRENT=$WP_FOLDER/Current
+if [ -z "$WALLPAPER_FOLDER" ]; then
+   WALLPAPER_FOLDER=$HOME/.wallpapers
+fi
+CURRENT=$WALLPAPER_FOLDER/Current
 COLOR_GEN=$HOME/.scripts/color_gen.py
 
 if [ -f $CURRENT ]; then
    rm $CURRENT
 fi
 
-WP_FILES=($WP_FOLDER/*)
+WP_FILES=($WALLPAPER_FOLDER/*)
 RANDOM_WP="${WP_FILES[RANDOM % ${#WP_FILES[@]}]}"
 
 ln -s "$RANDOM_WP" $CURRENT
 
 # generate/link the new colorscheme
-$HOME/.local/virtualenvs/playground/bin/python2 $COLOR_GEN "$RANDOM_WP"
-xrdb -merge $XDG_CONFIG_HOME/X11/Xresources
+#$HOME/.local/virtualenvs/playground/bin/python2 $COLOR_GEN "$RANDOM_WP"
+#xrdb -merge $XDG_CONFIG_HOME/X11/Xresources
 
 feh --bg-scale --no-fehbg $CURRENT
