5e7bee474b7433e3d692baaaea05f62a3781bfa6
Jeff <jostendorf@gmail.com>
2013-09-09 21:17:16 -0500

    Added imgur pull script and updated main script
    Now calling `wallpaper update` will pull the latest images from the imgur
    album.  This means that the folder should be maintained mainly against the
    imgur album.

--- wallpaper/wallpaper.sh
+++ wallpaper/wallpaper.sh
@@ -3,8 +3,18 @@
 if [ -z "$WALLPAPER_FOLDER" ]; then
    WALLPAPER_FOLDER=$HOME/.wallpapers
 fi
+
+if [ $# -gt 0 ]; then
+   if [ "$1" = "update" ]; then
+      $(dirname $(realpath $0))/imgur_update.py
+      exit 0
+   elif [ "$1" = "count" ]; then
+      echo "Wallpaper Count: $(ls $WALLPAPER_FOLDER | grep jpg | wc -l)"
+      exit 0
+   fi
+fi
+
 CURRENT=$WALLPAPER_FOLDER/Current
-COLOR_GEN=$HOME/.scripts/color_gen.py
 
 if [ -f $CURRENT ]; then
    rm $CURRENT
@@ -16,6 +26,7 @@ RANDOM_WP="${WP_FILES[RANDOM % ${#WP_FILES[@]} + 1]}"
 ln -s "$RANDOM_WP" $CURRENT
 
 # generate/link the new colorscheme
+#COLOR_GEN=$HOME/.scripts/color_gen.py
 #$HOME/.local/virtualenvs/playground/bin/python2 $COLOR_GEN "$RANDOM_WP"
 #xrdb -merge $XDG_CONFIG_HOME/X11/Xresources
 
