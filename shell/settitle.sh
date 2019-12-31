63f0a2b7cacb72b0a58b26d7bbe93dab2a035dbe
Jeff <jdost@users.noreply.github.com>
2019-12-30 23:10:18 -0800

    [settitle] Clean up script
    Updates to comply with more of the newer patterns I use

--- shell/settitle.sh
+++ shell/settitle.sh
@@ -1,17 +1,12 @@
 #!/bin/sh
 
-TITLE=""
-if [[ $# == 0 ]]; then
-   TITLE=$(tty | cut -d/ -f 3-)
-else
-   TITLE=$*
-fi
+set -euo pipefail
 
-if [[ -z "$TITLE" ]]; then
-   echo "Something went wrong..."
-   exit 1
-elif [[ -z "$TMUX" ]]; then
-   echo -ne "\033]0;$TITLE\007"
+args=$*
+title=${all:=$(tty | cut -d/ -f3-)}
+
+if [[ -z "${TMUX:-}" ]]; then
+   exec echo -ne "\033]0;$title\007"
 else
-   printf "\033k$TITLE\033\\"
+   exec printf "\033k$title\033\\"
 fi
