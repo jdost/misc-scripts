47a658b61ae1e7c865f3079019de1df026d57c4d
Jeff <jdost@users.noreply.github.com>
2019-12-30 22:34:30 -0800

    [wallpaper] Update grab script to python3

--- wallpaper/grab_wallpapers.py
+++ wallpaper/grab_wallpapers.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python2
+#!/usr/bin/env python3
 from os import getenv, listdir
 from os.path import join
-from urllib import urlretrieve
-from urllib2 import urlopen
+from urllib.request import urlretrieve
+from urllib.request import urlopen
 import json
 
 FOLDER = getenv("WALLPAPER_FOLDER", join(getenv("HOME"), ".wallpapers"))
@@ -13,13 +13,12 @@ IMG_EXT = ["." + ext for ext in ["jpg", "jpeg", "png", "gif"]]
 def get_img(url):
     fn = url.split('/')[-1]
     url = url if url.startswith("http") else ("http:" + url)
-    print "Downloading {} to {}".format(url, fn)
+    print("Downloading {} to {}".format(url, fn))
     urlretrieve(url, join(FOLDER, fn))
 
 if __name__ == '__main__':
     images_raw = urlopen(URL_BASE.format("list.json"))
-    images = dict(map(
-        lambda x: (x, URL_BASE.format(x)), json.load(images_raw)))
+    images = dict([(x, URL_BASE.format(x)) for x in json.load(images_raw)])
 
     for wp in listdir(FOLDER):
         if not any([lambda ext: wp.endswith(ext) for ext in IMG_EXT]):
@@ -27,6 +26,6 @@ if __name__ == '__main__':
         if wp in images:
             del images[wp]
 
-    for url in images.values():
+    for url in list(images.values()):
         url = "http:" + url if not url.startswith("http") else url
         get_img(url)
